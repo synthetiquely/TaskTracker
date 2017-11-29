c7d4f5de70db705788bcc5b030164c14a8091846
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2017-11-29 14:50:55 +0300

    Sorting and search markup for tasks

--- src/components/Task/TasksGrid.vue
+++ src/components/Task/TasksGrid.vue
@@ -4,6 +4,38 @@
       <i class="archive icon"></i>
       Tasks
     </h4>
+    <div class="ui secondary menu">
+      <a class="item">
+        <div class="ui basic button">
+          <i class="sort icon"></i> Waiting</div>
+      </a>
+      <a class="item">
+        <div class="ui pink button">
+          <i class="sort icon"></i> Assigned to you</div>
+      </a>
+      <a class="item">
+        <div class="ui orange button">
+          <i class="sort icon"></i> Implementation</div>
+      </a>
+      <a class="item">
+        <div class="ui yellow button">
+          <i class="sort icon"></i> Verifying</div>
+      </a>
+      <a class="item">
+        <div class="ui green button">
+          <i class="sort icon"></i> Releasing</div>
+      </a>
+      <div class="right menu">
+        <div class="item">
+          <div class="ui icon input">
+            <input type="text" placeholder="Search...">
+            <i class="search link icon"></i>
+          </div>
+        </div>
+      </div>
+      <app-create-project-modal v-if="showModal" @close="showModal = false">
+      </app-create-project-modal>
+    </div>
     <div class="ui items">
       <app-project-task-preview></app-project-task-preview>
       <app-project-task-preview></app-project-task-preview>
